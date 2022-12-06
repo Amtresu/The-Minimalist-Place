2a10749e78a82d623fd508fc09a44049e4cd2077
Amtresu <conor.follette@gmail.com>
2022-12-06 21:42:17 +0900

    cleaned migrations

--- db/migrate/20221123041144_remove_post_id_from_likes.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostIdFromLikes < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :likes, :PostId, :integer
-  end
-end
--- db/migrate/20221124033516_fix_likes_column.rb
+++
@@ -1,5 +0,0 @@
-class FixLikesColumn < ActiveRecord::Migration[7.0]
-  def self.up
-    rename_column :likes, :posts_id, :post_id
-  end
-end
