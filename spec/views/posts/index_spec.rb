9ea5c8b51d130150fdf143914e2175932d7c9018
Amtresu <conor.follette@gmail.com>
2022-12-06 18:19:15 +0900

    Added integration tests for posts, added eager loading for displaying comments

---
+++ spec/views/posts/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Post Index', type: :feature do
+  before :each do
+    @img = 'Img'
+    @author = User.create(name: 'Oscar Funk', PostsCounter: 2, photo: @img, bio: 'Drow Ranger')
+    @post1 = Post.create(author: @author, title: '1', text: 'Post 1', CommentsCounter: 1, LikesCounter: 1)
+    @post2 = Post.create(author:@author, title: '2', text: 'Post 2', CommentsCounter: 1, LikesCounter: 1)
+
+    visit user_path(@author.id)
+  end
+
+  it 'shows the username of the user' do
+    expect(page).to have_content(@author.name)
+  end
+
+  it 'Shows the number of posts from each user' do
+    expect(page.body).to include('2')
+  end
+
+  it 'Shows the posts title' do
+    expect(page.body).to have_content(@post1.title)
+  end
+
+  it 'Should show the post body' do
+      expect(page).to have_content(@post2.text)
+  end
+
+  it 'Redirects to show page when clicked' do
+    visit user_post_path(@author, @post1)
+    expect(current_path).to match user_post_path(@author, @post1)
+  end
+end
---
+++ spec/views/posts/show_spec.rb
@@ -0,0 +1,35 @@
+RSpec.describe 'Post Show', type: :feature do
+    before :each do
+      @img = 'Img'
+      @author = User.create(id: 1, name: 'Oscar Funk', PostsCounter: 2, photo: @img, bio: 'Drow Ranger')
+      @post1 = Post.create(id: 1, author: @author, title: 'title', text: 'Post 1', CommentsCounter: 2, LikesCounter: 2)
+    
+      @comment1 = Comment.create(author: @author, post: @post1, text: 'comment 1')
+  
+      visit user_post_path(@author, @post1)
+    end
+
+    it 'Should show the posts title' do 
+        expect(page).to have_content(@post1.title)
+    end
+
+    it 'Should include authors name' do 
+         expect(page).to have_content(@author.name)
+     end
+
+    it 'Should show the comments counter' do 
+        expect(page).to have_content(@post1.CommentsCounter)
+    end
+
+    it 'Should show the likes counter' do 
+        expect(page).to have_content(@post1.LikesCounter)
+    end
+
+    it 'Should show the authors name' do 
+        expect(page).to have_content(@comment1.author.name)
+    end
+
+    it 'Should show the authors comment' do 
+        expect(page).to have_content(@comment1.text)
+    end
+end
