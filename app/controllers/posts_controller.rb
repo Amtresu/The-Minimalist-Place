eb19b9939e1ba63279bedd7c130c007ac1ad4940
Amtresu <conor.follette@gmail.com>
2022-12-06 18:19:27 +0900

    fixed eager loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
-    @comments = @post.comments
+    @comments =  @post.comments.includes([:author])
   end
 
   def new
